264b0a748decb817a6f9c09d47ccd861c4c47cfe
Anto <vinishanto@gmail.com>
2023-05-21 23:53:04 +0530

    added hero banner- WIP

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -1,7 +1,15 @@
 export default function HomePage() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="container mx-auto bg-grad-primary pb-[100px] rounded-t-2xl">
+      <h1 className="text-3xl text-white pt-[100px] text-center">
+        Agri Commodities Trade, <br />
+        <span className="font-bold">Reimagined! 🚜</span>
+      </h1>
+      <p className="text-[#FFFEFF] opacity-70 text-center pt-5">
+        Enabling global wholesalers, manufacturers and retailers
+        <br />
+        source efficiently.
+      </p>
+    </div>
   );
 }
--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Header() {
   return (
     <div className="container mx-auto">
-      <Image src={HorizontalLogo} />
       <div className="float-right">RightSide</div>
+      <Image src={HorizontalLogo} />
     </div>
   );
 }
