5d982a741c235cf03e685f02c2040538769367ae
Anto <vinishanto@gmail.com>
2023-05-21 23:52:30 +0530

    added font

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -1,8 +1,12 @@
 import Header from "@/components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import "@/styles/custom.scss";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Header />
         {children}
       </body>
