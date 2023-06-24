ca56a820badc7a3fa2b7fad535de00bb732df55f
Anto <vinishanto@gmail.com>
2023-06-24 18:08:24 +0530

    added basic GTM

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -8,6 +8,7 @@ import Footer from "@/components/Footer";
 // import { store } from './app/store'
 import { Provider } from "react-redux";
 import { store } from "./store";
+import Script from "next/script";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
@@ -23,12 +24,40 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable}`}>
+      <head>
+        {/* Adding Google Tag manager Script 
+            Google analytycs will be configured within GTM
+        */}
+        <Script
+          strategy="afterInteractive"
+          id="GTMContainer"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NVRG4Q4');`,
+          }}
+        ></Script>
+        {/* End of Tag manager Script */}
+      </head>
       <body className="font-poppins text-black">
         <Provider store={store}>
           <Header />
           {children}
           <Footer />
         </Provider>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVRG4Q4"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
     </html>
   );
