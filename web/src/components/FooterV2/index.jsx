4537ae2605391df113e02389eda71b32258620e3
Anto <vinishanto@gmail.com>
2023-07-02 15:33:34 +0530

    removed comments

--- web/src/components/FooterV2/index.jsx
+++ web/src/components/FooterV2/index.jsx
@@ -38,34 +38,5 @@ export default function Footer() {
         </div>
       </div>
     </footer>
-    //    <div className="ml-150 flex justify-between">
-    //     <div className="my-24">
-    //       <HectarLogo alt="Footer Logo" className="w-[112px] lg:w-162" />
-    //     </div>
-    //     <div>
-    //       <FooterShip />
-    //     </div>
-    //   </div>
-    //   <div className="h-px bg-[#0000001A] mx-150"></div>
-    //   <div className="flex justify-between">
-    //     <div className="flex ml-150">
-    //       <div className="pr-7 py-7">About</div>
-    //       <div className="pr-7 py-7">Blog</div>
-    //       <div className="pr-7 py-7">Contact us</div>
-    //       <div className="pr-7 py-7">We're Hiring</div>
-    //     </div>
-    //     <div className="mr-150 flex my-7">
-    //       <div className="h-6 w-6 mr-7">
-    //         <TwitterImage />
-    //       </div>
-    //       <div className="h-6 w-6 mr-7">
-    //         <LinkedImage />
-    //       </div>
-    //       <div className="h-6 w-6">
-    //         <YoutubeImage />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </footer>
   );
 }
